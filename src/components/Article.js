b5b97b75aaa4bec39deea0faa9a677c4f3b5a33c
joycewangui <joycewangui590@gmail.com>
2024-08-21 10:30:41 +0300

    adds components and props

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-function Article({title, date = "January 1, 1970", preview, minutes}) {
+function Article({title, date = "January 1, 1970", preview}) {
     return (
         <article>
             <h3>{title}</h3>
